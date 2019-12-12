e6743b02169cd493e533b81d47834d936fbb55ea
zicanl <zicanl@uci.edu>
2019-12-12 12:47:31 -0800

    create Default Level

--- django/contrib/auth/management/commands/createDefaultLevel.py
+++ django/contrib/auth/management/commands/createDefaultLevel.py
@@ -6,12 +6,16 @@ from django.contrib.auth.models import StaffLevel
 class Command(BaseCommand):
     help = 'Used to create a default StaffLevels.'
 
-    def __init__(self, *args, **kwargs):
-        user = StaffLevel(levelName="user", levelInt=-1)
-        superuser = StaffLevel(levelName="superuser", levelInt=0)
-        user.save()
-        superuser.save()
-
     def execute(self, *args, **options):
         self.stdin = options.get('stdin', sys.stdin)  # Used for testing
         return super().execute(*args, **options)
+
+    def handle(self, *args, **options):
+        # TODO: add comments to explain, initialize default user and superUser levels to database
+        try:
+            user = StaffLevel(levelName="user", levelInt=-1)
+            superuser = StaffLevel(levelName="superuser", levelInt=0)
+            user.save()
+            superuser.save()
+        except:
+            pass
